29ea0c638c02bd6c19547019bdf51f4ee4445689
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-07-17 23:03:50 +0800

    Create Traffic_Light.ino

---
+++ Traffic_Light.ino
@@ -0,0 +1,17 @@
+#define ledR(x) digitalWrite(14,x? HIGH:LOW)
+#define ledY(x) digitalWrite(12,x? HIGH:LOW)
+#define ledG(x) digitalWrite(13,x? HIGH:LOW)
+
+int led[4]={18, 19, 23, 5};
+
+void setup() 
+{ for(int i=0 ;i<=3 ;i++) pinMode(led[i],OUTPUT);
+  for(int i=12;i<=14;i++) pinMode(i,OUTPUT);
+}
+
+void loop() 
+{ ledY(0);  ledR(1);  delay(3000);
+  ledR(0);  ledG(1);  delay(3000);
+  ledG(0);  ledY(1);  delay(2000);
+}
+
