6f198d2680d8b3afd4a1faa80d2eb500cdfc5935
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-07-18 20:36:26 +0800

    Create Blynk_TaskScheduling.ino

---
+++ Blynk_TaskScheduling.ino
@@ -0,0 +1,48 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPL6Ph6HH6_E"
+#define BLYNK_TEMPLATE_NAME "Latihan IoT"
+#define BLYNK_AUTH_TOKEN "ozCIeidx7nX9VxJbYVYpF2XP5U7DX1wS"
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+BlynkTimer timer;
+
+char ssid[] = "JHChua@unifi";
+char pass[] = "adamdanial123";
+
+uint8_t LEDs[]={18,19,23,5};
+
+BLYNK_CONNECTED()
+{ Blynk.virtualWrite(V0,LOW);
+  Blynk.virtualWrite(V1,LOW);
+}
+BLYNK_WRITE(V0)
+{ uint8_t led=param.asInt();
+  digitalWrite(LEDs[0],led);
+}
+BLYNK_WRITE(V1)
+{ uint8_t pwm = param.asInt();
+  pwm = pwm*255/100;
+  Serial.println(pwm);
+  analogWrite(LEDs[1],pwm);
+}
+bool status=0;
+void kelip()
+{ digitalWrite(LEDs[2],status);
+  status=!status;
+}
+
+void setup()
+{ for(uint8_t i=0;i<4;i++)
+    pinMode(LEDs[i],OUTPUT);
+  
+  Serial.begin(9600);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  timer.setInterval(200L,kelip);
+}
+
+void loop()
+{ Blynk.run();
+  timer.run();
+}
