362fa508e6b4bbafd847137ec982ee9ae01c05ab
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-07-18 19:23:43 +0800

    Create DHT11_Basic.ino

---
+++ DHT11_Basic.ino
@@ -0,0 +1,19 @@
+#include <dhtESP32-rmt.h>
+float temperature = 0.0;
+float humidity = 0.0;
+
+void setup() 
+{ delay(1000);
+	Serial.begin(9600);
+}
+
+void loop() 
+{	uint8_t error=read_dht(temperature, humidity, 4, DHT11);
+	if(error)
+		Serial.println(error);
+	else
+	{	Serial.println(temperature);
+		Serial.println(humidity);
+	}
+	delay(3000);	
+}
